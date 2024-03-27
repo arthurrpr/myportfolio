04e39b9ffffb9b8853b70133f70b72613e43f0e2
arthurrpr <arthur.rprodovalho@gmail.com>
2024-03-26 21:55:29 -0300

    pagina home finalizada

--- portfolio/src/assets/components/Home/Home.jsx
+++ portfolio/src/assets/components/Home/Home.jsx
@@ -1,14 +1,16 @@
+import './Home.css'
+
 export default function Home(){
     return(
         <section>
-            <article>
+            <article id="left">
                 <div className="introduction">
                     <div className="introduction-icons">
-                        <img src="icons/linkedin.svg" alt="LinkedIn Icon" />
-                        <img src="icons/github.svg" alt="GitHub Icon" />
+                        <a href="https://linkedin.com/in/arthur-risso" target='_blank' ><img src="icons/linkedin.svg" alt="LinkedIn Icon" /></a>
+                        <a href="https://github.com/arthurrpr" target='_blank'><img src="icons/github.svg" alt="GitHub Icon" /></a>
                     </div>
                     <div className="introduction-text">
-                        <h1>Hello World! Im a <span>Front-End</span> Developer.</h1>
+                        <h1>Hello World! I&#39;m a <span>Front-End</span> Developer.</h1>
                         <p className="text-description">Hello! I am Arthur Risso, a passionate about Front-End development.</p>
                     </div>
                 </div>
@@ -23,13 +25,13 @@ export default function Home(){
                     </div>
                 </div>
                 <div className="scroll">
-                    <img src="icons/mouse-icon.svg" alt="Mouse icon" />
+                    <img className='mouse' src="icons/mouse-icon.svg" alt="Mouse icon" />
                     <p>Scroll down</p>
                     <img src="icons/arrow-down-icon.svg" alt="" />
                 </div>
             </article>
-            <article>
-                <img src="img/home-img.png" alt="Home image" />
+            <article id="right">
+                <img src="img/home-img.png" alt="Home image" /> 
             </article>
         </section>
     )
